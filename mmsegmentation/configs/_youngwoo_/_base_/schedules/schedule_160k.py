65391208a8d46c149ea9f26d7ad946c4faa69f62
wowo0709 <wjsduddn0709@naver.com>
2022-04-30 06:58:01 +0000

    [EXP] Second exp: deeplabv3plus/deeplabv3plus_r50-d8_4x4_512x512_40k_custom_focal.py
    - 두번째 실험 결과 없로드 (issue #16)
    - custom.py: reduce_zero_label=True 삭제
    - schdules 파일: interval 수정
    - 기타 불필요한 파일 삭제
    issue #16

--- mmsegmentation/configs/_youngwoo_/_base_/schedules/schedule_160k.py
+++ mmsegmentation/configs/_youngwoo_/_base_/schedules/schedule_160k.py
@@ -5,5 +5,5 @@ optimizer_config = dict()
 lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
 # runtime settings
 runner = dict(type='IterBasedRunner', max_iters=160000)
-checkpoint_config = dict(by_epoch=False, interval=16000)
-evaluation = dict(interval=16000, metric='mIoU', pre_eval=True)
+checkpoint_config = dict(max_keep_ckpts=3, by_epoch=False, interval=8000)
+evaluation = dict(interval=8000, metric='mIoU', pre_eval=True)
