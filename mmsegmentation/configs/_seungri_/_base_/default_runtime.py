d8a87d1db553277fa581b17299585f2ff0339b65
seungriyou <seungri0826@gmail.com>
2022-05-04 04:25:45 +0000

    [Exp] Upload personal config folder
    실험에 사용하는 config 파일 중 `_base_` 폴더 내 파일들을 업로드합니다.
    issue #40

---
+++ mmsegmentation/configs/_seungri_/_base_/default_runtime.py
@@ -0,0 +1,34 @@
+# yapf:disable
+log_config = dict(
+    interval=50,
+    hooks=[
+        dict(type="TextLoggerHook", by_epoch=True),
+        # mlflow
+        dict(
+            type="MlflowLoggerHook",
+            exp_name="",
+        ),
+        # wandb
+        dict(
+            type="WandbLoggerHook",
+            init_kwargs=dict(
+                project="",  # lv2-p-3, semantic_segmentation
+                entity="",  # seungri0826, snow-man
+                name="",
+            ),
+        ),
+    ],
+)
+
+# fp16 settings
+optimizer_config = dict(type="Fp16OptimizerHook", loss_scale=512.0)
+# fp16 placeholder
+fp16 = dict()
+
+# yapf:enable
+dist_params = dict(backend="nccl")
+log_level = "INFO"
+load_from = None
+resume_from = None
+workflow = [("train", 1)]
+cudnn_benchmark = True
